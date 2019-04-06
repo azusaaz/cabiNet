ac18a0a61e1b89c0da6e5da1fde6d633b461d32d
“az” <shimazaki@aaa.email.ne.jp>
2019-04-06 14:48:03 -0400

    set apollo

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
+import ContentList from './Components/ContentList.js';
+
+const client = new ApolloClient({
+  uri:'http://localhost:8080/graphql'
+})
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-       
-      </div>
+
+      <ApolloProvider client={client}>
+        <div className="App">
+        <h1>cabiNet</h1>
+        <ContentList />
+        </div>
+      </ApolloProvider>
     );
   }
 }
---
+++ client/src/Components/ContentList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ContentList extends Component {
+  render() {
+    return (
+      <div id="ContentList">
+       <li>Content name</li>
+      </div>
+    );
+  }
+}
+
+export default ContentList;
